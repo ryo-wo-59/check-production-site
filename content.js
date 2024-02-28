4a9a3d90f3e14e05a0e80308ae4a2849fff30d96
ryo-wo-59 <ryo.aoki.wo@gmail.com>
2024-02-29 08:31:24 +0900

    修正

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     var header = document.getElementsByClassName('header');
     const divDom = document.createElement('div');
-    divDom.innerHTML = "テスト";
-    header.appendChild(divDom);
+    divDom.setAttribute('style', 'position: absolute; width: 50px; height: 50px; background-color: #ff0000; color: #FFF; line-height:50px; text-align: center; font-weight: bold;')
+    divDom.innerHTML = "本番";
+    console.log(divDom);
+    header[0].appendChild(divDom);
   };
