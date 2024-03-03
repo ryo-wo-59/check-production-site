dcac30493428fd4791e9b089da4ab8bc98923b5c
ryo-wo-59 <ryo.aoki.wo@gmail.com>
2024-03-03 20:12:36 +0900

    修正

--- content.js
+++ content.js
@@ -3,6 +3,5 @@ window.onload = function() {
     const divDom = document.createElement('div');
     divDom.setAttribute('style', 'position: fixed; top: 0px; left: 0px; z-index: 99999; text-align:center; width: 100px; height: 50px; background-color: #ff0000; color: #FFF; line-height:50px; text-align: center; font-weight: bold;')
     divDom.innerHTML = "本番";
-    console.log(divDom);
     body[0].appendChild(divDom);
   };
